5a02a18baf76c37a813985d1c0455f913dd26e7e
Marlena Śliwińska <marlenasliwinska@gmail.com>
2021-04-25 14:20:36 +0200

    initial HTML

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,18 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log('HELLO 🚀');
+
+fetch('https://api.github.com/users/marlena-sliwinska/repos')
+  .then(response => response.json())
+  .then(response => {
+      for (let repo of response)
+      {    
+          const {name, description, html_url} = repo;
+     
+        console.log(`Lala ${name} lala ${description} lala ${html_url}`);
+      }
+  }
+    
+    )
+  .catch(error => {console.log('error');})

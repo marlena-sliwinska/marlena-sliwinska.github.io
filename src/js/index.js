0b4d1d283aa6ef4219829f9eb3d4d5207fcb93b0
Marlena Śliwińska <marlenasliwinska@gmail.com>
2021-06-28 14:42:49 +0200

    projects added

--- src/js/index.js
+++ src/js/index.js
@@ -13,34 +13,46 @@ fetch('https://api.github.com/users/marlena-sliwinska/repos')
   .then(response => {
       for (let repo of response)
       {    
-          const {name, description, html_url, homepage} = repo;
+         const {name, description, html_url, homepage} = repo;
      
-          console.log(`Project named ${name} - ${description}. 
+         console.log(`Project named ${name} - ${description}. 
           You can find link to repository here:  i srodowisko: ${homepage}`);
-          const portfolio = document.querySelector('.project__box--js');
-          const myBox = ` <tr>
-          <td class="projects__description">project:</td>
-          <td class="projects__content projects__content--name">${name}</td>
-        </tr>
-        <tr>
-          <td class="projects__description">description:</td>
-          <td class="projects__content">${description}</td>
-        </tr>
-        <tr>
-          <td class="projects__description">demo:</td>
-          <td class="projects__content">
-            <
-            <a href="${homepage}" target="_blank"
-            rel="noopener noreferrer">see here</a>
-            >
-          </td>
-        </tr>
-        <tr>
-          <td class="projects__description">github:</td>
-          <td class="projects__content" ><<a href="${html_url}" target="_blank"
-          rel="noopener noreferrer">source code</a>></td>
-        </tr>`
-          portfolio.innerHTML +=  myBox;
+        const portfolio = document.querySelector('.project__box--js');
+        const myBox = `
+        <article class="projects__box ">
+        <table class="projects__table">
+            <tr>
+            <td class="projects__description">project:</td>
+            <td class="projects__content projects__content--name">${name}</td>
+          </tr>
+          <tr>
+            <td class="projects__description">description:</td>
+            <td class="projects__content"
+            style="padding-bottom: 16px;"
+            >${description}</td>
+          </tr>
+          <tr>
+          
+            <td class="projects__description">demo:</td>
+            <td class="projects__content"><<a
+              style ="color: #14B5D0"
+              href="${homepage}"
+              target="_blank"
+              rel="noopener noreferrer"
+              >see here</a>></td>
+          </tr>
+          <tr>
+            <td class="projects__description">github:</td>
+            <td class="projects__content"><<a 
+              style ="color: #14B5D0"
+              href="${html_url}"
+              target="_blank"
+              rel="noopener noreferrer"
+              >source code</a>></td>
+          </tr>
+        </table>
+        </article>`;
+        portfolio.innerHTML +=  myBox;
         }
   }
     
